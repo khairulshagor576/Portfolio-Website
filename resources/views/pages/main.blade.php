d803b8237824d446c5c5fd4e5d8600a4c69ee678
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-29 23:33:56 +0600

    create a model and migration file for main part and design in main part how to display data from database value

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -7,6 +7,27 @@
                 <li class="breadcrumb-item"><a href="{{route('admin.dashbord')}}">Dashboard</a></li>
                 <li class="breadcrumb-item active">Main</li>
             </ol>
+            <div class="row">
+                 <div class=" col-md-4">
+                     <h4>Background Image</h4>
+                     <img style="height: 30vh;" class="img-thumbnail" src="{{asset('assets/img/header-bg.jpg')}}">
+                     <input type="file" id="bc_img" name="bc_img">
+                 </div>
+                <div class="form-group col-md-4">
+                    <div>
+                        <label><h4>Title</h4></label>
+                        <input type="text" class="form-control" name="title" id="title">
+                    </div>
+                    <div>
+                        <label><h4>Sub Title</h4></label>
+                        <input type="text" class="form-control" name="sub_title" id="sub_title">
+                    </div>
+                    <div>
+                        <h4>File Upload</h4>
+                        <input type="file" class="mt-2" name="resume" id="resume">
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 @endsection
