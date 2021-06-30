0dd10d758bc2888617e665be6f8d0b5340a6c12c
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-30 08:44:40 +0600

    pdf and image file link between storage and pblic and create a main model and a migration file for database and those data show in main blade page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Main;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
    public function main()
    {
-       return view('pages.main');
+       $main = Main::first();
+       return view('pages.main',['main'=>$main]);
    }
 }
--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin_layout');
+@extends('layouts.admin_layout')
 @section('content')
     <main>
         <div class="container-fluid">
@@ -10,19 +10,19 @@
             <div class="row">
                  <div class=" col-md-4">
                      <h4>Background Image</h4>
-                     <img style="height: 30vh;" class="img-thumbnail" src="{{asset('assets/img/header-bg.jpg')}}">
-                     <input type="file" id="bc_img" name="bc_img">
+                     <img style="height: 30vh;" class="img-thumbnail" src="{{url($main->bc_image)}}">
+                     <input class="mt-2" type="file" id="bc_img" name="bc_img">
                  </div>
                 <div class="form-group col-md-4">
                     <div>
                         <label><h4>Title</h4></label>
-                        <input type="text" class="form-control" name="title" id="title">
+                        <input type="text" class="form-control" name="title" id="title" value="{{$main->title}}">
                     </div>
-                    <div>
+                    <div class="mt-2">
                         <label><h4>Sub Title</h4></label>
-                        <input type="text" class="form-control" name="sub_title" id="sub_title">
+                        <input type="text" class="form-control" name="sub_title" id="sub_title" value="{{$main->subtitle}}">
                     </div>
-                    <div>
+                    <div class="mt-2">
                         <h4>File Upload</h4>
                         <input type="file" class="mt-2" name="resume" id="resume">
                     </div>
